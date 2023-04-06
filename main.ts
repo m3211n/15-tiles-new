4a202df1bb672108d0ae60db324701dca04de0a4
m3211n <130044007+m3211n@users.noreply.github.com>
2023-04-06 16:17:33 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,12 @@
 namespace SpriteKind {
     export const Tile = SpriteKind.create()
 }
-function resetTiles (indexes: number[]) {
-    indexesOld = indexes
-    indexesNew = []
-    for (let index = 0; index <= 15; index++) {
-        console.log(indexesOld)
-        candidate = indexesOld._pickRandom()
-        indexesNew.push(candidate)
-        indexesOld.removeAt(indexesOld.indexOf(candidate))
-        console.log(indexesNew)
-    }
-    return indexesNew
-}
-function placeTiles (tilesIndexes: number[]) {
+function placeTiles (indexes: number[]) {
     X0 = 3
     Y0 = 2
     X = 0
     Y = 0
-    for (let value of tilesIndexes) {
+    for (let value of indexes) {
         tiles.placeOnTile(playTiles[value], tiles.getTileLocation(X + X0, Y + Y0))
         X += 1
         if (X == 4) {
@@ -46,35 +34,27 @@ function createTiles () {
     sprites.create(assets.image`tile14`, SpriteKind.Tile),
     sprites.create(assets.image`noTile`, SpriteKind.Tile)
     ]
-    placedTiles = [
-    sprites.create(assets.image`placedTile0`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile1`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile2`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile3`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile4`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile5`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile6`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile7`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile8`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile9`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile10`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile11`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile12`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile13`, SpriteKind.Tile),
-    sprites.create(assets.image`placedTile14`, SpriteKind.Tile),
-    sprites.create(assets.image`noTile`, SpriteKind.Tile)
-    ]
 }
-let placedTiles: Sprite[] = []
+function shuffleIndexes (indexes: number[]) {
+    indexesOld = indexes
+    indexesNew = []
+    for (let index = 0; index <= 15; index++) {
+        candidate = indexesOld._pickRandom()
+        indexesNew.push(candidate)
+        indexesOld.removeAt(indexesOld.indexOf(candidate))
+    }
+    return indexesNew
+}
+let candidate = 0
+let indexesNew: number[] = []
+let indexesOld: number[] = []
 let playTiles: Sprite[] = []
 let Y = 0
 let X = 0
 let Y0 = 0
 let X0 = 0
-let candidate = 0
-let indexesNew: number[] = []
-let indexesOld: number[] = []
 let tilesIndexes: number[] = []
+createTiles()
 info.setScore(0)
 info.startCountdown(120)
 scene.setBackgroundColor(12)
@@ -82,9 +62,5 @@ tiles.setCurrentTilemap(tilemap`level1`)
 for (let index = 0; index <= 15; index++) {
     tilesIndexes[index] = index
 }
-console.log("----------------")
-console.log(tilesIndexes)
-createTiles()
-tilesIndexes = resetTiles(tilesIndexes)
+tilesIndexes = shuffleIndexes(tilesIndexes)
 placeTiles(tilesIndexes)
-console.log(tilesIndexes)
